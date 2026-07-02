c84f755fcbf32228bce1b0cb23332a7ff1d6f5aa
ham22-tester <148444409+ham22-tester@users.noreply.github.com>
2026-07-02 22:58:18 +0900

    Update tca9555_test.c

--- src/tca9555_test.c
+++ src/tca9555_test.c
@@ -1,6 +1,20 @@
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/logging/log.h>
 
-int tca9555_test_init(void)
+LOG_MODULE_REGISTER(tca9555_test, LOG_LEVEL_INF);
+
+#ifdef CONFIG_TCA9555_TEST
+
+static int tca9555_test_init(void)
 {
+    LOG_INF("TCA9555 Test Driver Loaded");
+
     return 0;
 }
+
+SYS_INIT(tca9555_test_init, APPLICATION, 90);
+
+#endif

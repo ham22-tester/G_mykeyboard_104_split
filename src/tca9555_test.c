2729b3dcde9b65c0b81425a66d69a633cff53972
ham22-tester <148444409+ham22-tester@users.noreply.github.com>
2026-07-02 23:51:59 +0900

    Update tca9555_test.c

--- src/tca9555_test.c
+++ src/tca9555_test.c
@@ -1,20 +1,24 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/devicetree.h>
-#include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(tca9555_test, LOG_LEVEL_INF);
 
-#ifdef CONFIG_TCA9555_TEST
+#define TCA_NODE DT_NODELABEL(tca9555_u1)
 
-static int tca9555_test_init(void)
+static int init(void)
 {
-    LOG_INF("TCA9555 Test Driver Loaded");
+    const struct device *dev = DEVICE_DT_GET(TCA_NODE);
+
+    if (!device_is_ready(dev)) {
+        LOG_ERR("TCA9555 NOT READY");
+        return -1;
+    }
+
+    LOG_INF("TCA9555 FOUND!");
 
     return 0;
 }
 
-SYS_INIT(tca9555_test_init, APPLICATION, 90);
-
-#endif
+SYS_INIT(init, APPLICATION, 90);
